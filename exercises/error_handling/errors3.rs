535de6048a177d3a5f4ae87b83aceaa965047eb2
kaioh33 <gupajibril@gmail.com>
2022-05-01 22:29:48 +0100

    adding my solution

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,11 +4,11 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` for hints!
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
@@ -20,6 +20,7 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
