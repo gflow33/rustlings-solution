353787f2da19dccaf1397fd81fa26a70fe2c27b2
kaioh33 <gupajibril@gmail.com>
2022-05-05 09:53:36 +0100

    adding my solution

--- exercises/option/option1.rs
+++ exercises/option/option1.rs
@@ -1,23 +1,34 @@
 // option1.rs
 // Make me compile! Execute `rustlings hint option1` for hints
 
-// I AM NOT DONE
 
 // you can modify anything EXCEPT for this function's signature
 fn print_number(maybe_number: Option<u16>) {
-    println!("printing: {}", maybe_number.unwrap());
+    match maybe_number {
+        Some(num) => println!("printing: {}", num),
+        None => println!("printing: None"),
+}
+
+    if maybe_number.is_some() {
+        println!("'maybe_number' is a number!")
+    }
+
+    if maybe_number.is_none() {
+        println!("'maybe_number' is NOT a number")
+    }
 }
 
 fn main() {
-    print_number(13);
-    print_number(99);
+    print_number(Some(13));
+    print_number(Some(99));
+    print_number(None);
 
-    let mut numbers: [Option<u16>; 5];
+    let mut numbers: [Option<u16>; 5] = [None; 5];
     for iter in 0..5 {
         let number_to_add: u16 = {
             ((iter * 1235) + 2) / (4 * 16)
         };
 
-        numbers[iter as usize] = number_to_add;
+        numbers[iter as usize] = Some(number_to_add);
     }
 }
